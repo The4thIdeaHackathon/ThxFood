9dbd1f9645d63a848e01f318bb52e28531b80bd1
sangmin <dlaak0630@naver.com>
2018-05-14 14:43:14 +0900

    delete

--- review-service/bin/www.js
+++
@@ -1,7 +0,0 @@
-const app = require('../index');
-
-console.log("app :", app);
-app.listen(3001, () => (
-    console.log("Running")
-))
-
--- sharing-service/bin/www.js
+++
@@ -1,7 +0,0 @@
-const app = require('../index');
-
-console.log("app :", app);
-app.listen(3000, () => (
-    console.log("Running")
-));
-
